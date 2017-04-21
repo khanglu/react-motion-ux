dcff871fdc8738f95f16842640381c10c1775b0e
Khang Lu <khang.lu@cheproximity.com.au>
2017-04-21 11:29:12 +1000

    Add slider for value change

--- demos/demo1-value-change/ValueChange.jsx
+++ demos/demo1-value-change/ValueChange.jsx
@@ -1,10 +1,47 @@
 import React, { Component } from 'react'
 import { Motion, spring } from 'react-motion'
 
-const ValueChange = () => (
-  <Motion defaultStyle={{x: 0}} style={{x: spring(10, {stiffness: 120, damping: 40})}}>
-    {value => <div>{Math.round(value.x)}</div>}
-  </Motion>
-)
+
+class ValueChange extends Component {
+  constructor () {
+    super()
+    this.state = {
+      currentValue: 0,
+      oldValue: 0
+    }
+    this.onValueChange = this.onValueChange.bind(this)
+  }
+
+  onValueChange (newValue) {
+    this.setState ((prevState, props) => {
+      return {
+        currentValue: newValue,
+        oldValue: prevState.currentValue
+      }
+    })
+  }
+
+  render () {
+    const { currentValue, oldValue } = this.state 
+    return (
+      <div>
+        <input 
+          type="range"
+          value={currentValue}
+          min="0"
+          max="50"
+          step="10"
+          onChange={(e) => this.onValueChange(Number(e.target.value))}
+        />
+        <Motion 
+          defaultStyle={{x: oldValue}}
+          style={{x: spring(currentValue, {stiffness: 120, damping: 40})}}
+        >
+          {motionValue => <div id="value-label">{Math.round(motionValue.x)}</div>}
+        </Motion>
+      </div>
+    )
+  }
+}
 
 export default ValueChange
